a5dafd2cb48f0fb8591fcf23965f3969403d6c50
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2023-01-08 10:18:54 +0200

    I will cry, missing ';'

--- include/soc/msm8909.h
+++ include/soc/msm8909.h
@@ -1,6 +1,6 @@
 #ifndef MSM8909_H_	/* Include guard */
 #define MSM8909_H_
 
-extern void clean_fb(volatile char *fb, int width, int height, int stride)
+extern void clean_fb(volatile char *fb, int width, int height, int stride);
 
 #endif // MSM8909_H_
